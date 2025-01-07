7b139e933d1e22aeed3070c87d1981410282632d
Mostafa Habib <momosasahabib9@gmail.com>
2025-01-07 15:07:17 +0200

    Video 3 - Equality In Struct & Class

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -36,42 +36,53 @@
             #endregion
 
             #region Video 2
-            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7 };
-            int Index = Helper.SearchArray(Numbers, 5);
-            Console.WriteLine(Index);
+            //int[] Numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            //int Index = Helper.SearchArray(Numbers, 5);
+            //Console.WriteLine(Index);
 
-            Point[] points = new Point[]
-            {
-                new Point(10,20),
-                new Point(30,40),
-                new Point(50,60),
-                new Point(70,80),
-                new Point(90,100),
-            };
-            Point point3 = new Point(50, 60);
-            Index = Helper.SearchArray(points, point3);
-            Console.WriteLine(Index);
+            //Point[] points = new Point[]
+            //{
+            //    new Point(10,20),
+            //    new Point(30,40),
+            //    new Point(50,60),
+            //    new Point(70,80),
+            //    new Point(90,100),
+            //};
+            //Point point3 = new Point(50, 60);
+            //Index = Helper.SearchArray(points, point3);
+            //Console.WriteLine(Index);
 
-            Employee[] employees = new Employee[]
-            {
-                new Employee{ID = 1, Name = "Ahmed", Salary = 1000},
-                new Employee{ID = 2, Name = "Ali", Salary = 2000},
-                new Employee{ID = 3, Name = "Omar", Salary = 3000},
-                new Employee{ID = 4, Name = "Sara", Salary = 4000},
-            };
-            Employee employee3 = new Employee { ID = 5, Name = "Nora", Salary = 5000 };
-             Index = Helper.SearchArray(employees, employee3);
-            Console.WriteLine(Index);
-            Employee employee = new Employee() { ID = 1, Name = "Mostafa", Salary = 1000 };
-            Employee employee2 = new Employee() { ID = 1, Name = "Mostafa", Salary = 1000 };
-            if (employee.Equals(employee2))
-            {
-                Console.WriteLine("Equal");
-            }
-            else
-            {
-                Console.WriteLine("Not Equal");
-            }
+            //Employee[] employees = new Employee[]
+            //{
+            //    new Employee{ID = 1, Name = "Ahmed", Salary = 1000},
+            //    new Employee{ID = 2, Name = "Ali", Salary = 2000},
+            //    new Employee{ID = 3, Name = "Omar", Salary = 3000},
+            //    new Employee{ID = 4, Name = "Sara", Salary = 4000},
+            //};
+            //Employee employee3 = new Employee { ID = 5, Name = "Nora", Salary = 5000 };
+            // Index = Helper.SearchArray(employees, employee3);
+            //Console.WriteLine(Index);
+            //Employee employee = new Employee() { ID = 1, Name = "Mostafa", Salary = 1000 };
+            //Employee employee2 = new Employee() { ID = 1, Name = "Mostafa", Salary = 1000 };
+            //if (employee.Equals(employee2))
+            //{
+            //    Console.WriteLine("Equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Equal");
+            //}
+            #endregion
+
+            #region Video 3
+            Employee emp1 = new Employee { ID = 1000, Name = "Ahmed", Salary = 1};
+            Employee emp2 = new Employee { ID = 1, Name = "Ahmed", Salary = 1000 };
+            Console.WriteLine(emp1.GetHashCode());
+            Console.WriteLine(emp2.GetHashCode());
+            //if (emp1.Equals(emp2))
+            //    Console.WriteLine("Equals");
+            //else
+            //    Console.WriteLine("Not Equals");
             #endregion
         }
     }
