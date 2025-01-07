22391ec325ee9bb3c0942737cfd6ede18938444a
Mostafa Habib <momosasahabib9@gmail.com>
2025-01-07 13:00:35 +0200

    Video 1 - Generics - Swap

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Video 1
+            int A = 10 , B = 20;
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine();
+            Helper<int>.Swap(ref A, ref B);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+
+            Console.WriteLine();
+            
+            double x = 10.5, y = 20.5;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine();
+            Helper<double>.Swap(ref x, ref y);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.WriteLine();
+
+            Point p1 = new Point(10, 20);
+            Point p2 = new Point(30, 40);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine();
+            Helper<Point>.Swap(ref p1, ref p2);
+            Console.WriteLine(p1); 
+            Console.WriteLine(p2);
+            #endregion
         }
     }
 }
